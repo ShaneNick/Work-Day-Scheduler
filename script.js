a7008ecb4a43dd18981a61e04a3138f1f04877ee
ShaneNick <shanetomonia@gmail.com>
2023-06-13 10:13:37 -0400

    added js

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Display the current date in the header of the page.
+  let currentDay = dayjs().format('D MMM YYYY');
+  $('#currentDay').text(currentDay);
+
+  $('.time-block').each(function() {
+    let hour = parseInt($(this).attr('id').split('-')[1]);
+    let currentHour = dayjs().hour();
+  
+    // Convert hour to 24-hour format
+    if (hour < 9) { // if hour is less than 9 (i.e., 1-8), it's in the afternoon
+      hour += 12;
+    }
+    // Remove any existing classes
+    $(this).removeClass('past present future');
+
+    // Add appropriate class
+    if (hour < currentHour) {
+      $(this).addClass('past');
+    } else if (hour === currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+
+    // Get any saved events from local storage
+    let event = localStorage.getItem('hour-' + hour);
+    if (event) {
+      $(this).find('.description').val(event);
+    }
+  });
+
+  // Add a listener for click events on the save button
+  $('.saveBtn').click(function() {
+    let hour = $(this).parent().attr('id').split('-')[1];
+    let event = $(this).siblings('.description').val();
+
+    // Save the event in local storage
+    localStorage.setItem('hour-' + hour, event);
+  });
 });
