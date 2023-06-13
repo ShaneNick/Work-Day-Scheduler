347b3461cdec8dc95b2a02ccb1f7dd4490798542
Shane Tomonia <121068293+ShaneNick@users.noreply.github.com>
2023-06-13 10:28:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(function () {
-  // Display the current date in the header of the page.
   let currentDay = dayjs().format('D MMM YYYY');
   $('#currentDay').text(currentDay);
 
@@ -8,13 +7,11 @@ $(function () {
     let currentHour = dayjs().hour();
   
     // Convert hour to 24-hour format
-    if (hour < 9) { // if hour is less than 9 (i.e., 1-8), it's in the afternoon
+    if (hour < 9) { 
       hour += 12;
     }
-    // Remove any existing classes
     $(this).removeClass('past present future');
 
-    // Add appropriate class
     if (hour < currentHour) {
       $(this).addClass('past');
     } else if (hour === currentHour) {
@@ -23,19 +20,16 @@ $(function () {
       $(this).addClass('future');
     }
 
-    // Get any saved events from local storage
     let event = localStorage.getItem('hour-' + hour);
     if (event) {
       $(this).find('.description').val(event);
     }
   });
 
-  // Add a listener for click events on the save button
   $('.saveBtn').click(function() {
     let hour = $(this).parent().attr('id').split('-')[1];
     let event = $(this).siblings('.description').val();
 
-    // Save the event in local storage
     localStorage.setItem('hour-' + hour, event);
   });
 });
